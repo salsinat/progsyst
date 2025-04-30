1567d46c47cdd5518deb7891ead9ffd99b78d06e
stanislas <stanislas.guilleray9@etu.univ-lorraine.fr>
2025-04-30 16:58:44 +0200

    fin td1

--- sigint_test.c
+++ sigint_test.c
@@ -2,4 +2,14 @@
 #include <signal.h>
 #include <unistd.h>
 
-void
+void afficherSignal(int sig) {
+	printf("\nSignal reçu : %d\n", sig);
+}
+
+int main() {
+	signal(SIGINT, afficherSignal);
+	printf("PID : %d - Appuyez sur Crtl+C\n", getpid());
+	while(1)
+		sleep(1);
+	return 0;
+}
